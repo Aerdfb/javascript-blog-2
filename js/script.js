6e4db15f8978e14efafb8e63107d6fa2dbfb9e8c
Dominik Krawczyk <dominix.kr@gmail.com>
2021-11-04 19:30:06 +0100

    add active class to current article

--- js/script.js
+++ js/script.js
@@ -7,8 +7,10 @@
 
 const titleClickHandler = function(event){
     console.log('Link was clicked');
+    event.preventDefault();
     const clickedElement = this;
     console.log(event);
+
 
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -17,7 +19,7 @@ const titleClickHandler = function(event){
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
     console.log('clickedElement (with plus): ' + clickedElement);
 
@@ -30,11 +32,18 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const hrefLink = clickedElement.getAttribute('href');
+    console.log(hrefLink);
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
+    const currentArticle = document.querySelector(hrefLink);
+
     /* add class 'active' to the correct article */
+
+    currentArticle.classList.add('active');
 
 }
 
